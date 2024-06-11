9f73446b23401fd55a7ea0dcc063fb1805120f6c
AlejandroManrique <alejomanri09@gmail.com>
2024-06-11 18:51:54 -0500

    Update sketch.js
    obstacles

--- sketch.js
+++ sketch.js
@@ -1,12 +1,22 @@
 var trex, trex_running, edges, trex_collided;
 var groundImage, invisibleGround, groundImage;
-var cloud, cloudsGroups, cloudImage;
-var nerImage;
+var cloud, cloudsGroup, cloudImage;
+var newImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+var score;
 
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   groundImage = loadImage("ground2.png");
   cloudImage = loadImage("cloud.png");
+
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
+
 }
 
 function setup(){
@@ -15,6 +25,7 @@ function setup(){
   //crear sprite de Trex
   trex = createSprite(50,160,20,50);
   trex.addAnimation("running", trex_running);
+  trex.addAnimation("collided" , trex_collided);
   edges = createEdgeSprites();
   
   //agregar tamaño y posición al Trex
@@ -28,12 +39,16 @@ function setup(){
 
   invisibleGround = createSprite(200,190,400,10);
   invisibleGround.visible =false;
+
+  score = 0;
 }
 
 
 function draw(){
   //establecer color de fondo.
   background("white");
+  text("Puntuación: "+ score, 500,50);
+  score = score + Math.round(frameCount/60);
   
   //cargar la posición Y del Trex
   console.log(trex.y)
@@ -53,17 +68,49 @@ function draw(){
   trex.collide(invisibleGround);
 
   spawnClouds();
+
+  spanwObstacles();
   
   drawSprites();
 }
 
-function spawnClouds() {
-  cloud = createCanvas(600,100,40,10);
-  cloud.addImage(cloudImage);
-  cloud.y = Maht.round(random(10,60));
-  cloud.scale = 0,4;
-  cloud.velocityX= -3;
+function spanwObstacles(){
+  if (frameCount % 60 === 0){
+    var obstacle = createSprite(400,165,10,40);
+    obstacle.velocityX = -6;
 
-  cloud.depth = trex.depth;
-  trex.depth = tres.depth + 1;
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      case 2: obstacle.addImage(obstacle2);
+              break;
+      case 3: obstacle.addImage(obstacle3);
+              break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;        
+    }
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+  }
+}
+
+function spawnClouds() {
+  if (frameCount % 60 === 0) { 
+    cloud = createSprite(600,100,40,10);
+    cloud.addImage(cloudImage);
+    cloud.y = Math.round(random(10,60));
+    cloud.scale = 0.4;
+    cloud.velocityX= -3;
+
+    cloud.lifetime = 200;
+
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
+  }
 }
